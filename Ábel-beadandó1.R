77bf5f960519da84589b85139cb4e63a6fdc098e
LantosAbel <96252679+LantosAbel@users.noreply.github.com>
2021-12-16 16:58:05 +0100

    Add files via upload

---
+++ Ábel-beadandó1.R
@@ -0,0 +1,36 @@
+#4. Hozz létre egy fgv-t, amelyik data.frame-t vár bemenetként, illetve két oszlop (változó) nevet 
+#és kimenetként a két változóra készít egy scatterplot-t, amit kirajzol. 
+#Opcionálisan lehessen tetszõleges "title"-t hozzáadni, illetve képként menteni is az eredményt.
+
+
+ess= read.spss("ESS6_HUN_autotranslate.sav",
+                rownames= F,
+                Stringsasfactors= T,
+                tolower= F,
+                as.data.frame= T,
+                reencode= T)
+ess= data.frame (ess)
+
+
+#csak a plot
+fgv = function (m,c,d) {
+  if (is.data.frame(m)) {
+     
+      plot (x=c, y=d,type="p")
+  }
+
+}
+
+fgv1 (ess,ess$trstep,ess$trstun,"karmazsin","rossz")
+fgv (ess,ess$trstep,ess$trstun)
+
+#az egész egybe
+fgv1 = function (m,c,d,t,k) {
+  if (is.data.frame(m)) {
+    plot (x=c, y=d,type="p")
+  }
+    t=title(main=t)
+      k=png ("k") + dev.off(k)
+}
+
+
